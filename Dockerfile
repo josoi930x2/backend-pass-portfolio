e6d2d9b5c40ebfaf9eebd0b75bbdb6983460ef04
josoi <josoi930x2@gmail.com>
2025-11-19 02:45:28 +0900

    workflow test commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM openjdk:17-jdk-slim
+# openjdk:17-jdk-slim 강의 원문코드
+FROM amazoncorretto:17-alpine
+
 WORKDIR /app
 COPY build/libs/*.jar app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"] 
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BackendportfolioApplication {
+    //first commit
     public static void main(String[] args) {
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
